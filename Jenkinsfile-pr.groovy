7df5a327623f9b247717ad08cea795a18b8b578f
diegoviana89 <33573953+diegoviana89@users.noreply.github.com>
2023-05-18 18:12:02 -0500

    Update Jenkinsfile-pr.groovy

--- Jenkinsfile-pr.groovy
+++ Jenkinsfile-pr.groovy
@@ -1,50 +1,38 @@
-def executeStage = "no"
+    def executeStage = "no"
 pipeline {
     agent any
 
     stages {
-
-        stage('Deploy to INT?') {
-            agent none
+		stage('Stage 1') {
             steps {
                 script {
                     executeStage = input(
                         id: 'executeStage',
                         message: 'Do you want to deploy version to DEV?',
                         parameters: [
-                                choice(defaultValue: false, choices: ["yes", "no"],description: 'yes to confirm/empty to skip this stage?', name: 'Yes')
-                        ])
+                                choice(defaultValue: false, choices: ['yes', 'no'],description: 'yes to confirm/empty to skip this stage?', name: 'Yes')
+                        ]
+                )
+                
+                if(executeStage=='yes'){
+                    when {
+                    expression { executeStage == 'yes' }
                     }
+                    echo 'Primer stage'
+                }else{
+                    
+                    echo 'Skipped'
+                    currentBuild.result = "SUCCESS"
                 }
-            }
 
-		stage('Stage 1') {
-            when {
-                expression { executeStage == "yes" }
-            }
-            steps {
-                echo 'Primer stage'
+                }
             }
         }
-
-        stage('Deploy to DEV?') {
-            agent none
-            steps {
-                script {
-                    executeStage = input(
-                        id: 'executeStage',
-                        message: 'Do you want to deploy version to DEV?',
-                        parameters: [
-                                choice(defaultValue: false, choices: ["yes", "no"],description: 'yes to confirm/empty to skip this stage?', name: 'Yes')
-                        ])
-                    }
-                }
-            }
          
         
         stage('Stage 2') {
             when {
-                expression { executeStage == "yes" }
+                expression { executeStage == 'yes' }
             }
             steps {
                 echo 'Segundo stage'

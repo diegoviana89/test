9a6ce1983f51d703ae35efb956764ca8a6cdf771
diegoviana89 <33573953+diegoviana89@users.noreply.github.com>
2023-05-18 17:48:36 -0500

    Update Jenkinsfile-pr.groovy

--- Jenkinsfile-pr.groovy
+++ Jenkinsfile-pr.groovy
@@ -4,23 +4,23 @@ pipeline {
     stages {
 		stage('Stage 1') {
             steps {
-        script {
-          def executeStage = input(
-                  id: 'executeStage',
-                  message: 'Do you want to deploy version to DEV?',
-                  parameters: [
-                         choice(defaultValue: false, choices: ['yes', 'no'],description: 'yes to confirm/empty to skip this stage?', name: 'Yes')
-                  ]
-          )
-          if(executeStage=='yes'){
-             echo 'Primer stage'
-          }else{
-             echo 'Skipped'
-            currentStage.result = "NOT_BUILT"
-          }
+                script {
+                def executeStage = input(
+                        id: 'executeStage',
+                        message: 'Do you want to deploy version to DEV?',
+                        parameters: [
+                                choice(defaultValue: false, choices: ['yes', 'no'],description: 'yes to confirm/empty to skip this stage?', name: 'Yes')
+                        ]
+                )
+                if(executeStage=='yes'){
+                    echo 'Primer stage'
+                }else{
+                    echo 'Skipped'
+                    currentBuild.result = "SUCCESS"
+                }
 
-        }
-      }
+                }
+            }
         }
          
         

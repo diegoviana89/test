9f1f0261500044eb4978717ba60081c4c767c0bd
diegoviana89 <33573953+diegoviana89@users.noreply.github.com>
2023-05-18 16:53:52 -0500

    Update Jenkinsfile-pr.groovy

--- Jenkinsfile-pr.groovy
+++ Jenkinsfile-pr.groovy
@@ -5,25 +5,9 @@ pipeline {
         
         stage('Stage 1') {
             steps {
-                script {
-                    // Solicitar confirmación al usuario
-                    def userInput = input(
-                        id: 'userConfirmation',
-                        message: 'Deseas ejecutar el Stage 1',
-                        parameters: [
-                            [$class: 'GeneratorChoiceParameterDefinition', choices: 'Si\No', description: 'Selecciona una opcion']
-                        ]
-                    )
-                    
-                    // Verificar la respuesta del usuario
-                    if (userInput == 'Si') {
-                        echo 'Ejecutando Stage 1...'
-                        // Agrega aquí las instrucciones para el Stage 1
-                    } else {
-                        echo 'Omitiendo Stage 1...'
-                    }
-                }
+                echo 'Hola'
             }
+
         }
         
         stage('Stage 2') {

4830172f0caf703c4310ba1cdd2862f40a839707
diegoviana89 <33573953+diegoviana89@users.noreply.github.com>
2023-05-18 17:17:57 -0500

    Update Jenkinsfile-pr.groovy

--- Jenkinsfile-pr.groovy
+++ Jenkinsfile-pr.groovy
@@ -3,20 +3,17 @@ pipeline {
 
     stages {
 		
-		stage('do optional stuff?') {
-			userInput = input(
-				id: 'userInput', message: "Some important question?", parameters: [
-				booleanParam(defaultValue: false, description: 'really?', name: 'myValue')
-			])
-		}
-
-		stage('optional: do magic') {
-			if (userInput) {
-				echo "do magic"
-			} else {
-				// do what ever you want when skipping this build
-				currentBuild.result = "UNSTABLE"
-			}
-		}
+		stage('Stage 1') {
+            steps {
+                echo 'Segundo stage'
+            }
+        }
+         
+        
+        stage('Stage 2') {
+            steps {
+                echo 'Segundo stage'
+            }
+        }
     }
 }

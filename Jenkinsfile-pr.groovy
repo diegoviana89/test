b853487e5a87816d0daa6848db4df12a2dd79d86
diegoviana89 <33573953+diegoviana89@users.noreply.github.com>
2023-05-18 18:09:46 -0500

    Update Jenkinsfile-pr.groovy

--- Jenkinsfile-pr.groovy
+++ Jenkinsfile-pr.groovy
@@ -23,9 +23,23 @@ pipeline {
                 expression { executeStage == "yes" }
             }
             steps {
-                echo 'Segundo stage'
+                echo 'Primer stage'
             }
         }
+
+        stage('Deploy to DEV?') {
+            agent none
+            steps {
+                script {
+                    executeStage = input(
+                        id: 'executeStage',
+                        message: 'Do you want to deploy version to DEV?',
+                        parameters: [
+                                choice(defaultValue: false, choices: ["yes", "no"],description: 'yes to confirm/empty to skip this stage?', name: 'Yes')
+                        ])
+                    }
+                }
+            }
          
         
         stage('Stage 2') {

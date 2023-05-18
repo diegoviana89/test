503c7d9a2cd30b760311632452ab82ab6bb6796e
diegoviana89 <33573953+diegoviana89@users.noreply.github.com>
2023-05-18 15:13:23 -0500

    Create Jenkinsfile-pr.groovy

---
+++ Jenkinsfile-pr.groovy
@@ -0,0 +1,17 @@
+pipeline {
+    agent any
+    
+    stages {
+        stage('Stage 1') {
+            steps {
+                echo 'Hola'
+            }
+        }
+        
+        stage('Stage 2') {
+            steps {
+                echo 'Segundo stage'
+            }
+        }
+    }
+}
